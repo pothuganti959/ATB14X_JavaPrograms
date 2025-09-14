3ed9577f1b50e81b5a466845428be6189f0ab658
Aishwarya <pothuganti.aishwarya@gmail.com>
2025-09-14 18:35:29 +0530

    moved code to new package

--- src/practice/Cancel.java
+++ src/practice/Cancel.java
@@ -1,3 +1,5 @@
+package practice;
+
 public class Cancel {
     public static void main(String[] args) {
         System.out.println("cancel");
--- src/practice/Sample.java
+++ src/practice/Sample.java
@@ -1,3 +1,5 @@
+package practice;
+
 public class Sample
 {
     public static void main(String[] args) {
